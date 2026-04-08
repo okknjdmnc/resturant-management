4d8dc6e208285ad2d9bbf39dc7660bf7445c6694
unknown <you@example.com>
2026-04-09 00:11:05 +0800

    Remove utils from tracking

--- utils/supabase_client.py
+++
@@ -1,9 +0,0 @@
-from supabase import create_client, Client
-import os
-
-def get_supabase_client() -> Client:
-    url = os.environ.get('SUPABASE_URL')
-    key = os.environ.get('SUPABASE_KEY')
-    return create_client(url, key)
-
-supabase: Client = get_supabase_client()
